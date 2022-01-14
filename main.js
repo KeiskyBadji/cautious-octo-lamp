130210b5d6ece826deef42dbfebf85c8af31c318
KeiskyBadji <91973679+KeiskyBadji@users.noreply.github.com>
2022-01-14 19:59:21 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+/*let a  = Number(prompt("Первое число", '1'))
+let b  = +prompt('Второе число', '2')
+
+alert(a + b)
+
+alert('')*/
+
+const text = document.querySelector('h1')
+
+
+
+text.style.color = 'red'
+text.style.fontSize = '30px'
+
+function clickColor() {
+    text.style.color = 'green'
+    text.style.fontSize = '120px'
+}
